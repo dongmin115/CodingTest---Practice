2c9146f0d62266143625aa485162521e639b5a54
dongminLim <137386209+dongmin115@users.noreply.github.com>
2024-09-24 19:29:50 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.20 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -0,0 +1,12 @@
+function solution(arr1, arr2) {
+    var answer = new Array(arr1.length).fill(0).map(()=> new Array(arr2[0].length).fill(0));
+    
+    for(let i=0;i<arr1.length;i++){
+        for(let j=0;j<arr2[0].length;j++){
+            for(let k=0;k<arr1[0].length;k++){
+                answer[i][j] += arr1[i][k] * arr2[k][j];
+            }
+        }
+    }
+    return answer;
+}
