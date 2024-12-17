3301ec9c474457b465511bf52a7d615efa3ab760
dongminLim <137386209+dongmin115@users.noreply.github.com>
2024-12-17 17:49:48 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,15 @@
+function solution(n)
+{
+    let count = 0;
+    
+    while(n > 0){
+        if(n % 2 === 0){
+            n /= 2;
+        } else {
+            count++;
+            n--;
+        }
+    }
+    
+    return count;
+}
