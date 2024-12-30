24893b6be932a9115d31c2500206a93b731b8df1
dongminLim <137386209+dongmin115@users.noreply.github.com>
2024-12-30 22:25:12 +0900

    [Silver IV] Title: 30, Time: 184 ms, Memory: 15600 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.js
+++ 백준/Silver/10610. 30/30.js
@@ -2,31 +2,18 @@ const input = require('fs')
     .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
     .toString()
     .trim()
+    .split('')
+    .map(Number);
 
+function solution() {
+    input.sort((a,b) => b - a);
+    const sum = input.reduce((a,b) => a + b, 0);
 
-function solution(input){
-
-    let arr = input.split('').map(Number);
-    let sum = 0;
-
-    // 30의 배수이려면 0이 포함되어 있어야 한다
-    if(!arr.includes(0)){
-        return console.log(-1);
+    if(sum % 3 !== 0 || !input.includes(0)){
+        console.log(-1);
+    } else {
+        console.log(input.join(''));
     }
-
-    // 30의 배수이려면 모든 숫자의 합이 3의 배수여야 한다
-    for(let i=0; i<arr.length; i++) {
-        sum += arr[i];
-    }
-    if( sum % 3 !== 0 ){
-        return console.log(-1);
-    }
-
-    // 새롭게 알게 된 기능 : 내림차순 간단히 정렬하는 법 arr.sort((a,b) => b-a)
-    // 오름차순 정렬은 arr.sort((a,b) => a-b)
-    // 가장 큰 수를 만들어야 하므로 내림차순 정렬
-    console.log(arr.sort((a,b) => b-a).join('').toString());
-
 }
 
-solution(input);
+solution();
