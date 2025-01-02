a79007b12ca1af1bf279eeffef788858037367fb
dongminLim <137386209+dongmin115@users.noreply.github.com>
2025-01-03 01:50:57 +0900

    [level 2] Title: 짝지어 제거하기, Time: 68.09 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,14 @@
+function solution(s)
+{   
+    const stack = [];
+    
+    for(let i=0; i<s.length; i++){
+        if(s[i] === stack[stack.length -1]){
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+    
+    return stack.length === 0 ? 1 : 0;
+}
