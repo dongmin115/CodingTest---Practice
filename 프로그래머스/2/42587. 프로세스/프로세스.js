b27aaf85ca4cdc85e76810d191bfd4d77fa75dab
dongminLim <137386209+dongmin115@users.noreply.github.com>
2025-01-16 16:48:45 +0900

    [level 2] Title: 프로세스, Time: 2.42 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,17 +1,4 @@
 function solution(priorities, location) {
-    let queue = priorities.map((priority, idx) => [priority, idx]);
-    let result = 0;
-
-    while (true) {
-        const [currPriority, currIndex] = queue.shift();
-
-        if (queue.some(([p]) => p > currPriority)) {
-            queue.push([currPriority, currIndex]);
-        } else {
-            result++;
-            if (currIndex === location) {
-                return result;
-            }
-        }
-    }
+    var answer = 0;
+    return answer;
 }
